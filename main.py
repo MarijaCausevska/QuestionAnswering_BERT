11f2decbb9798927910621d0c865c8aeb67907af
MarijaCausevska <43523151+MarijaCausevska@users.noreply.github.com>
2021-02-11 17:06:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,11 +46,12 @@ if __name__ == '__main__':
             loss = outputs[0]
             loss.backward()
             optim.step()
+            directory = "saved_models/"
+            path = directory + f"saved_model.pth"
+            torch.save(model.state_dict(),path) 
 
     model.eval()
-    directory = "saved_models/"
-    path = directory + f"saved_model.pth"
-    torch.save(model.state_dict(),path) 
+   
 train_path = keras.utils.get_file("train.json", "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json")
 eval_path = keras.utils.get_file("eval.json", "https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json")
 train = pd.read_json(train_path)
